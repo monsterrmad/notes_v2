94e2a7a69830c6c62f15c6e311e3739ab7ab4e69
Den <zolotoverkhdenis@gmail.com>
2021-09-13 23:30:02 +0300

    fixes

--- note/models.py
+++ note/models.py
@@ -27,7 +27,7 @@ class Note(models.Model):
     body = HTMLField(null=True, blank=True)
 
     date_created = models.DateTimeField(auto_now=True, blank=True)
-    date_edited = models.DateTimeField(blank=True, default=timezone.now())
+    date_edited = models.DateTimeField(blank=True, default=timezone.now)
     public = models.BooleanField(default=False, blank=True)
     favorite = models.BooleanField(default=False, blank=True)
     views = models.IntegerField(default=0, blank=True)
