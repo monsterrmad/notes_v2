a1e562164cf89759c1971e6277cff614ad9767b3
Den <zolotoverkhdenis@gmail.com>
2021-10-11 22:58:06 +0300

    note admin page admin page navlink

--- note/admin.py
+++ note/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from note.models import Note
 
+
 # Note model registration for django admin
-admin.site.register(Note)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "public", "name", "user", "date_created")
+
+
+admin.site.register(Note, NoteAdmin)
